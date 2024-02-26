437be688092dc46ef0b43692de038466ab71beab
Dominic Ndondo <dominic.ndondo@outlook.com>
2024-02-26 13:42:44 -0500

    Calendar View in app's entry point.

--- CustomCalendar/CalendarView.swift
+++ CustomCalendar/CalendarView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 struct CalendarView: View {
     @State private var color: Color = .blue
     @State private var date: Date = .now
-    let daysOfWeek = ["M", "T", "W", "T", "F", "S", "S"]
+    let daysOfWeek = Date.capitalizedFirstLettersOfWeekdays
+    let columns = Array(repeating: GridItem(.flexible()), count: 7)
+    @State private var days: [Date] = []
     
     var body: some View {
         VStack {
@@ -20,8 +22,44 @@ struct CalendarView: View {
             LabeledContent("Date/Time") {
                 DatePicker("", selection: $date)
             }
+            //Calendar Header
+            HStack {
+                ForEach(daysOfWeek.indices, id: \.self) { index in
+                    Text(daysOfWeek[index])
+                        .fontWeight(.black)
+                        .foregroundStyle(color)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            
+            //Calendar days
+            LazyVGrid(columns: columns) {
+                
+                ForEach(days, id: \.self) { day in
+                    if day.monthInt != date.monthInt {
+                        Text("")
+                    } else {
+                        Text(day.formatted(.dateTime.day()))
+                            .fontWeight(.bold)
+                            .foregroundStyle(.secondary)
+                            .frame(maxWidth: .infinity, minHeight: 40)
+                            .background(
+                                Circle()
+                                    .foregroundStyle(
+                                Date.now.startOfDay == day.startOfDay ? .red.opacity(0.2) : color.opacity(0.2)
+                                )
+                            )
+                    }
+                }
+            }
         }
         .padding()
+        .onAppear {
+            days = date.calendarDisplayDays
+        }
+        .onChange(of: date) {
+            days = date.calendarDisplayDays
+        }
     }
 }
 
--- CustomCalendar/Date+Extensions.swift
+++ CustomCalendar/Date+Extensions.swift
@@ -6,3 +6,87 @@
 //
 
 import Foundation
+
+
+extension Date {
+    
+    static var capitalizedFirstLettersOfWeekdays: [String] {
+        let calendar = Calendar.current
+        let weekdays = calendar.shortWeekdaySymbols
+
+        return weekdays.map { weekday in
+            guard let firstLetter = weekday.first else { return "" }
+            return String(firstLetter).capitalized
+        }
+    }
+    
+    static var fullMonthNames: [String] {
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+
+        return (1...12).compactMap { month in
+            dateFormatter.setLocalizedDateFormatFromTemplate("MMMM")
+            let date = Calendar.current.date(from: DateComponents(year: 2000, month: month, day: 1))
+            return date.map { dateFormatter.string(from: $0) }
+        }
+    }
+    
+    //returns the start day for evry month
+    var startOfMonth: Date {
+        Calendar.current.dateInterval(of: .month, for: self)!.start
+    }
+    
+    //returns the end day of every month
+    // .end returns one day above the last day so we need to subtract one to get the true last date
+    var endOfMonth: Date {
+        let lastDay = Calendar.current.dateInterval(of: .month, for: self)!.end
+        return Calendar.current.date(byAdding: .day, value: -1, to: lastDay)!
+    }
+    
+    //returns the start day of the previous month
+    var startOfPreviousMonth: Date {
+        let dayInpreviousMonth = Calendar.current.date(byAdding: .month, value: -1, to: self)!
+        return dayInpreviousMonth.startOfMonth
+    }
+    
+    //number of days in a month
+    var numberOfDaysInMonth: Int {
+        Calendar.current.component(.day, from: endOfMonth)
+    }
+    
+    //sunday before start of month
+    var sundayBeforeStart: Date {
+        let startOfMonthWeekday = Calendar.current.component(.weekday, from: startOfMonth)
+        let numberFromPreviousMonth = startOfMonthWeekday - 1
+        return Calendar.current.date(byAdding: .day, value: -numberFromPreviousMonth, to: startOfMonth)!
+    }
+    
+    //Returns the proper days to be displayed by the calendar view based
+    var calendarDisplayDays: [Date] {
+        var days: [Date] = []
+        
+        //Current month days
+        for dayOffset in 0..<numberOfDaysInMonth {
+            let newDay = Calendar.current.date(byAdding: .day, value: dayOffset, to: startOfMonth)!
+            days.append(newDay)
+        }
+        
+        //Previous Month Days
+        for dayOffset in 0..<startOfPreviousMonth.numberOfDaysInMonth {
+            let newDay = Calendar.current.date(byAdding: .day, value: dayOffset, to: startOfPreviousMonth)!
+            days.append(newDay)
+        }
+        
+        return days.filter { $0 >= sundayBeforeStart && $0 <= endOfMonth }.sorted(by: < )
+        
+    }
+    
+    //returns the month number of that specific date
+    var monthInt: Int {
+        Calendar.current.component(.month, from: self)
+    }
+    
+    var startOfDay: Date {
+        Calendar.current.startOfDay(for: self)
+    }
+}
