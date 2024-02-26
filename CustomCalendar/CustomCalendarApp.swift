4d4d9d635e3b8faf7cf9d878899ae21fe2b511f1
Dominic Ndondo <dominic.ndondo@outlook.com>
2024-02-26 12:06:13 -0500

    Initial Commit

---
+++ CustomCalendar/CustomCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomCalendarApp.swift
+//  CustomCalendar
+//
+//  Created by DOMINIC NDONDO on 2/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
